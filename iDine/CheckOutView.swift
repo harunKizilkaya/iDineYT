c7608f451351fd00009845e20d2367ef7e8ac40e
Harun Kizilkaya <h.kizilkaya@web.de>
2023-04-24 17:51:57 +0200

    - add addLoyaltyDetails, loyaltyNumbers, tipAmounts, tipAmount in CheckOutView() for payment - add loyaltyPoints form which appears when isOn with animation - add a picker for tips

--- iDine/CheckOutView.swift
+++ iDine/CheckOutView.swift
@@ -13,12 +13,37 @@ struct CheckOutView: View {
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     @State private var paymentType = "Cash"
     
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    
+    let tipAmounts = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 15
     var body: some View {
-        VStack {
+        Form {
             Section {
                 Picker("How do you want to pay?", selection: $paymentType) {
                     ForEach(paymentTypes, id: \.self) {
                         Text($0)
+                    }
+                }
+                Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+                
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: $loyaltyNumber)
+                }
+            }
+            
+            Section("Add a tip?") {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                
+                Section("Total: $100") {
+                    Button("Confirm Order") {
+                        
                     }
                 }
             }
