659f7c41dbce7cc1a6a0e99be9d28fc251d400d9
Harun Kizilkaya <h.kizilkaya@web.de>
2023-04-24 17:56:54 +0200

    - create totalPrice computed variable which calculates all together

--- iDine/CheckOutView.swift
+++ iDine/CheckOutView.swift
@@ -18,6 +18,12 @@ struct CheckOutView: View {
     
     let tipAmounts = [10, 15, 20, 25, 0]
     @State private var tipAmount = 15
+    
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
     var body: some View {
         Form {
             Section {
@@ -40,11 +46,12 @@ struct CheckOutView: View {
                     }
                 }
                 .pickerStyle(SegmentedPickerStyle())
-                
-                Section("Total: $100") {
+            }
+            
+                Section("Total: \(totalPrice)") {
                     Button("Confirm Order") {
                         
-                    }
+                    
                 }
             }
         }
