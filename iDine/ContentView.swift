a9d71bf5b3b5b56541f31b598d7f31247cb13af8
Harun Kizilkaya <h.kizilkaya@web.de>
2023-04-24 15:53:23 +0200

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Harun Kizilkaya on 31.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(menu) { section in
+                    Text("Hello World!")
+                    
+                }
+                
+            }
+            .navigationTitle("Menu")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Harun Kizilkaya on 31.03.23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
